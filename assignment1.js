2234d3366debaca4d1efecf2cc178c0682329edb
Rishimp7 <rishimpt7@gmail.com>
2024-09-30 23:01:40 +0530

    Add files via upload
    Day1 & Day 2

---
+++ assignment1.js
@@ -0,0 +1,13 @@
+const { log } = require("console")
+
+var first_name = "Ponting"
+var company_name = "ACB"
+var mob_number = 9428422424
+var isAutomation = true
+var hasplaywright
+
+console.log(first_name,typeof first_name);
+console.log(company_name,typeof company_name);
+console.log(mob_number,typeof mob_number);
+console.log(isAutomation,typeof isAutomation);
+console.log(hasplaywright,typeof hasplaywright);
---
+++ assignment2.js
@@ -0,0 +1,16 @@
+const browserVersion = "Chrome"
+
+function getBrowserVersion ()
+{
+
+if (browserVersion== "Chrome")
+{
+    //var browserVersion = "Edge" //prints undefinded
+    let browserVersion = "firefox"
+
+    console.log("Browser version using let ", browserVersion);//prints firefox
+}
+
+console.log("Browser version using let ", browserVersion);
+}
+getBrowserVersion()
